4ef639395172ddc420f5ba6346334b2d01b7f3d8
xiaoyuhe67 <xiaoyuhe67@github.com>
2016-07-22 16:55:20 -0400

    webcrawler

---
+++ src/SpiderLeg.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SpiderLeg {
+	
+	private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+	
+	private List<String> links = new LinkedList<String>(); // Just a list of URLs
+	private Document htmlDocument; // This is our web page, or in other words, our document
+	
+	
+	
+	public boolean crawl(String url)
+    {
+        try
+        {
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
+            if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+                                                          // indicating that everything is great.
+            {
+                System.out.println("\n**Visiting** Received web page at " + url);
+            }
+            if(!connection.response().contentType().contains("text/html"))
+            {
+                System.out.println("**Failure** Retrieved something other than HTML");
+                return false;
+            }
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            System.out.println("Found (" + linksOnPage.size() + ") links");
+            for(Element link : linksOnPage)
+            {
+                this.links.add(link.absUrl("href"));
+                System.out.println(link.absUrl("href"));
+            }
+            return true;
+        }
+        catch(IOException ioe)
+        {
+            // We were not successful in our HTTP request
+            return false;
+        }
+    }
+	
+	
+	public boolean searchForWord(String searchWord)
+    {
+        // Defensive coding. This method should only be used after a successful crawl.
+        if(this.htmlDocument == null)
+        {
+            System.out.println("ERROR! Call crawl() before performing analysis on the document");
+            return false;
+        }
+        System.out.println("Searching for the word " + searchWord + "...");
+        String bodyText = this.htmlDocument.body().text();
+        return bodyText.toLowerCase().contains(searchWord.toLowerCase());
+    }
+
+	
+	public List<String> getLinks() // Returns a list of all the URLs on the page
+	{
+	  return this.links;
+	}
+}
